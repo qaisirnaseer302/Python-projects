27ab89950204ca257f6e0b82b04901ce6446cbaa
qaisirnaseer302 <qaisirnaseer302@gmail.com>
2025-07-07 20:57:47 +0500

    music file

---
+++ musiclibrary.py
@@ -0,0 +1,8 @@
+music={
+    "care" : "https://youtu.be/3DCLdEjgI1g?si=lKENLvu90B5TLyft",
+    "sorry" : "https://youtu.be/nmBhjE6CNqs?si=SIrNiAu7XhZZ3Eg_",
+    "broken" : "https://youtu.be/ULCFj4ahkgo?si=WIBkS3-ZonPZdY68",
+    "smile" : "https://youtu.be/W6HEKo_Zj5Y?si=FC7aqmfNq-NkhGzz",
+    "slowly" : "https://youtu.be/ml3vTanQPW4?si=wo9VD_atBgDCQkeT",
+    
+}
