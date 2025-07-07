8889ba5a8c39cf05b0f1fa4683ead17adfe2366b
qaisirnaseer302 <qaisirnaseer302@gmail.com>
2025-07-07 21:07:29 +0500

    comments added

--- jarvis.py
+++ jarvis.py
@@ -1,15 +1,20 @@
+#Moduels used in Jarvis
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
 import musiclibrary
 import pyjokes
 import datetime as dt
+
 joke=pyjokes.get_joke()
 engine=pyttsx3.init()
 
+# speak functin using pyttsx3 module
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+
+#This function is used to extract numbers from command    
 def num(command):
         operation=command.split(" ")
         numbers=len(operation)
@@ -18,15 +23,11 @@ def num(command):
         numbers+=2
         num2=int(operation[numbers])
         return num1 ,num2
-def dateTime(command):
-    if "time" in command:
-        time=dt.datetime.now().time().strftime("%H %m %p")
-        speak(time) 
-    elif "date" in command:
-        date=dt.datetime.now().date().strftime("%d %B %Y")
-        speak(date)
+
+#This function is used to respond on the base of command
 
 def processCommand(command):
+
     if "open google" in command:
         webbrowser.open("https://google.com")
 
@@ -37,23 +38,32 @@ def processCommand(command):
         webbrowser.open("https://facebook.com") 
     
     elif "open insta" in command :
-        webbrowser.open("https://instagram.com")    
-    
+        webbrowser.open("https://instagram.com") 
+
+    # to play songs
+
     elif command.startswith("play"):
+
         song=command.split(" ")[1]
         songList=musiclibrary.music
+
         if song in songList.keys():
             webbrowser.open(songList.get(song))
         else :
             print("song not in the provided list....")
+
+    # TELL A  JOKE 
+
     elif command.startswith("tell"):
         speak(joke)
         speak("ha ha ha")
+
+    #  PERFORM SOME BASIC MATHMATICAL FUNCTIONS   
+    #  
     elif "calculate" or "what is " in command :
         number1,number2=num(command)
+
         if "plus" in command:
-            # newCommand=command.replace("plus","+")  
-            # speak(f"the sum is {num1 + num2}")
             sum=number1+number2
             speak(f"The sum of {number1} and {number2} is {sum}")
             
@@ -80,50 +90,67 @@ def processCommand(command):
         elif "power" in command:
             power=number1**number2
             speak(f"{number1} raised to the power {number2} is {power}")  
-        elif "exit" in command:
-            print("GoodBye...")     
-    if "time" in command :
+          
+    # PERFORM TIME AND DATE RELATED BASIC FUNCTIONS
+    
+    elif "time" in command :
 
         time=dt.datetime.now().time().strftime("%H  %M  %p")
         speak(f"it is {time}")
+
     elif "date" in command:
         date=dt.datetime.now().date().strftime("%d %B %Y")
         speak(f"it is {date}")
+
     elif "day" in command:
         now=dt.datetime.now()
         day=now.strftime("%A")
         speak(f"today is {day}")
+
     elif "month" in command:
        month= dt.datetime.now().strftime("%B")
        speak(f"This is {month}")
+
     elif "year" in command:
         year=dt.datetime.now().strftime("%Y")
         speak(f"This is {year}")
+
     else :
         print("did not understand....")
 
 print("wellcome....")
 speak("Initializing Jarvis")
+
 while True :
+
     r=sr.Recognizer()
     print("recognizing....")
+
     try :
         with sr.Microphone() as source:
+
             r.adjust_for_ambient_noise(source, duration=1)
             print("Listening....")
             audio=r.listen(source,timeout=2,phrase_time_limit=3)
-            word=r.recognize_google(audio).lower()    
+            word=r.recognize_google(audio).lower()  
+
         if word == "jarvis" :
+                
                 speak("Ya")
                 print("Jarvis Active...")
+
                 with sr.Microphone() as source:
                     r.adjust_for_ambient_noise(source, duration=1)
                     audio=r.listen(source,timeout=2,phrase_time_limit=3)
                     command=r.recognize_google(audio).lower()
+
                     if command.startswith("exit"):
                         break
                     else :
                         processCommand(command)
+
+        elif word == "exit":
+            break
 
     except Exception as e :
         print(e)
